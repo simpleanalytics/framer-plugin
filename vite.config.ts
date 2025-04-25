45d23c4ba9d9b4232d778fdeb5f877e0b13a1b94
Jean P.D. Meijer <jean@analoginterface.io>
2025-04-25 17:53:24 +0200

    feat: install Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import mkcert from "vite-plugin-mkcert"
 import framer from "vite-plugin-framer"
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), mkcert(), framer()],
+    plugins: [react(), mkcert(), framer(), tailwindcss()],
     build: {
         target: "ES2022",
     },
